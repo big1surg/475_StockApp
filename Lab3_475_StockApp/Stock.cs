74bba061d42b8181a7ac70746e46c26b59826398
Sergio Garcia <garcia.serg92@gmail.com>
2017-03-28 16:19:14 -0700

    Add files via upload

---
+++ Lab3_475_StockApp/Stock.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab3_475_StockApp {
+    /// <summary>
+    /// class creates stock, and when stock reaches a certain threshold is sends our messages to its subscribers
+    /// </summary>
+    class Stock {
+        string stockName; //name
+        int initialValue; //stock price
+        int maxChange; //stock price change
+        int notifyWhenThisAmount; //value to measure to raise event
+        int currentVal, count, x; //currentvalue of the stock, x is a variable for random number the stock goes up by, 
+            //count is num of times thredhols is reached
+        int numberChanges = 0; //keeps track of how many times the stock goes up
+        //static AutoResetEvent autoEvent;
+        public delegate void StockNotice(string stockName, int currentValue, int numberChanges); //delegate that sends information from
+            //one object to another
+        public event StockNotice StockEvent; //event to notify 
+        public event StockNotice SaveStocks; //event to save stock to a file 
+        Thread th; //thread
+
+        /// <summary>
+        /// default constructor
+        /// </summary>
+        public Stock() {
+            stockName = "";
+            initialValue = 0;
+            maxChange = 5;
+            notifyWhenThisAmount = 10;
+            th = new Thread(activate);
+            th.Start();
+        }
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="stockName">name of stock</param>
+        /// <param name="initialValue">starting value</param>
+        /// <param name="maxChange">amount the stock is permitted to go up by, 0-maxChange</param>
+        /// <param name="notifyWhenThisAmount">notify broker when it has reached this threhold</param>
+        public Stock(string stockName, int initialValue, int maxChange, int notifyWhenThisAmount) {
+            this.stockName = stockName;
+            this.initialValue = initialValue;
+            this.maxChange = maxChange;
+            this.notifyWhenThisAmount = notifyWhenThisAmount;
+            //autoEvent = new AutoResetEvent(false);
+            th = new Thread(activate); //the thread is created
+            th.Start(); //thread starts
+        }
+
+        /// <summary>
+        /// starts the thread increasing
+        /// </summary>
+        public void activate() {
+            //autoEvent.Set();
+            count = 0; //count is set to zero, increments each time threshold is reached
+            currentVal = initialValue; //set current to initial
+            Random r = new Random(); //create random 
+            while (th.IsAlive && numberChanges<=99) { //while thread is alive and count !=5 (count = times threshold is reached)
+                numberChanges++; //increase number of changes
+                Thread.Sleep(500); //sleep for 500 milliseconds
+                x = r.Next(0, maxChange); //set x equal to random value between 0 and maxChange
+                currentVal += x; //currentvalue = currentval + the new random value
+                if((currentVal - initialValue)>notifyWhenThisAmount) { //when difference is the threshold, enter this for loop
+                    count++; 
+                    //increase count
+                    //invoke event method 
+                     StockEvent(stockName, currentVal, numberChanges);
+                     SaveStocks(stockName, initialValue, currentVal);
+                }
+            }
+            th.Abort(); //kills thread
+        }
+       
+        /// <summary>
+        /// property for stock name
+        /// </summary>
+        public string StockName {
+            get { return stockName; }
+            set { stockName = value; }
+        }
+
+        /// <summary>
+        /// propert for initialValue
+        /// </summary>
+        public int InitialValue {
+            get { return initialValue; }
+            set { initialValue = value; }
+        }
+
+        /// <summary>
+        /// propert for maxchange
+        /// </summary>
+        public int MaxChange {
+            get { return maxChange; }
+            set { maxChange = value; }
+        }
+
+        /// <summary>
+        /// property for currentvalue
+        /// </summary>
+        public int CurrentValue {
+            get { return currentVal; }
+            set { currentVal = value; }
+        }
+
+        /// <summary>
+        /// property for number of changes
+        /// </summary>
+        public int NumberChanges {
+            get { return numberChanges; }
+        }
+
+        /// <summary>
+        /// property for notifying when threshold reached
+        /// </summary>
+        public int NotifyWhenThisAmount {
+            get { return notifyWhenThisAmount; }
+            set { notifyWhenThisAmount = value; }
+        }
+
+    }
+}
---
+++ Lab3_475_StockApp/StockBroker.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab3_475_StockApp {
+    /// <summary>
+    /// This class is for StockBroker
+    /// -it creates a stock broker who watches stock
+    /// </summary>
+    class StockBroker {
+
+        string brokerName; //name of broker
+        List<Stock> stockList = new List<Stock>(); //list of stock that the broker watches
+        //directory for the file
+        private const string dir = @"C:\Users\sinci\Documents\Visual Studio 2015\Projects\Lab3_475_StockApp\Lab3_475_StockApp\";
+        private const string path = dir + "TextFile1.txt";
+        //thread synchronization
+        EventWaitHandle waitHandle = new EventWaitHandle(true, EventResetMode.AutoReset, "SHARED_BY_ALL_PROCESSES");
+        private FileStream myFileStream;
+
+        /// <summary>
+        /// default constructor
+        /// </summary>
+        public StockBroker(){ brokerName = "";}
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="brokerName">brokers name</param>
+        public StockBroker(string brokerName) { this.brokerName = brokerName;}
+
+        /// <summary>
+        /// method to add stock to list of stock that the broker watches
+        /// also contains two event handlers
+        /// one event notifies when the stock has reached a certain threshold
+        /// one event saves to a textfile when the stock has reached a certain threshold
+        /// </summary>
+        /// <param name="stock">the stock that the broker is watching</param>
+        public void AddStock(Stock stock) {
+            stockList.Add(stock);
+            lock (stock) {
+                stock.StockEvent += new Stock.StockNotice(this.Notify); //the subscriber, subscribes to the delegate on stock
+                stock.SaveStocks += new Stock.StockNotice(this.SaveStock); //the subscriber, subscribes to the delegate on the stock
+            }
+        }
+
+        /// <summary>
+        /// event handler for the event when the threshold is reached
+        /// </summary>
+        /// <param name="name">stock name</param>
+        /// <param name="val">stock current value</param>
+        /// <param name="change">amount of times the stock changed before reaching the threshold</param>
+        public void Notify(string name, int val, int change) {
+            waitHandle.WaitOne(); //thread is locked
+            //display stock name, current value, and number of changes
+            Console.WriteLine("{0} {1} {2} {3}", brokerName.PadRight(20), name.PadRight(20), 
+                val.ToString().PadRight(20), change.ToString().PadRight(20));
+            waitHandle.Set(); //released
+        }
+
+        /// <summary>
+        /// event handler for the event when the threshold is reached
+        /// </summary>
+        /// <param name="name">stock name</param>
+        /// <param name="iniVal">initial value of stock</param>
+        /// <param name="currVal">current value of stock</param>
+        public void SaveStock(string name, int iniVal, int currVal) {
+            try { //lock
+                while (true) {
+                    try {
+                        myFileStream = new FileStream(path, FileMode.Append); //open file stream
+                        break;
+                    } catch {
+                        Thread.Sleep(20);
+                    }
+                }
+                StreamWriter outputFile = new StreamWriter(myFileStream);
+                //date and time, name of stock, initial value of stock, and current value of stock
+                outputFile.WriteLine("{0} {1} {2} {3}", System.DateTime.Now.ToString().PadRight(30),
+                        name.PadRight(20), iniVal.ToString().PadRight(10), currVal.ToString().PadRight(10));
+                //}
+                outputFile.Close();
+                myFileStream.Close();
+                //myFileStream.Dispose();
+            } finally {
+                if (myFileStream != null) {
+                    myFileStream.Dispose();
+                }
+            }
+        }
+    }
+}
